765f1694b5571e73149bc0f737361ec2ae029089
Baisayan <bbbbaisayan@gmail.com>
2026-07-02 13:11:24 +0000

    made git styled tree sessions inspired from pi

---
+++ src/agent/session/__init__.py
@@ -0,0 +1,46 @@
+from agent.session.entries import (
+    BaseSessionEntry,
+    BranchSummaryEntry,
+    CompactionEntry,
+    CustomEntry,
+    LabelEntry,
+    LeafEntry,
+    MessageEntry,
+    ModelChangeEntry,
+    SessionEntry,
+    SessionInfoEntry,
+    ThinkingLevelChangeEntry,
+)
+from agent.session.jsonl import (
+    SessionJsonlError,
+    entries_from_json_lines,
+    entry_from_json_line,
+    entry_to_json_line,
+)
+from agent.session.memory import SessionState
+from agent.session.storage import JsonlSessionStorage, SessionStorage
+from agent.session.tree import SessionTreeError, entries_by_id, path_to_entry
+
+__all__ = [
+    "BaseSessionEntry",
+    "BranchSummaryEntry",
+    "CompactionEntry",
+    "CustomEntry",
+    "JsonlSessionStorage",
+    "LabelEntry",
+    "LeafEntry",
+    "MessageEntry",
+    "ModelChangeEntry",
+    "SessionEntry",
+    "SessionInfoEntry",
+    "SessionJsonlError",
+    "SessionState",
+    "SessionStorage",
+    "SessionTreeError",
+    "ThinkingLevelChangeEntry",
+    "entries_by_id",
+    "entries_from_json_lines",
+    "entry_from_json_line",
+    "entry_to_json_line",
+    "path_to_entry",
+]
